ae00f7a9a5e5994a4a400cca068a52e955ff7a29
Folami Oluwapelumi <folamioluwapelumi1@gmail.com>
2022-07-26 20:31:22 -0700

    mediaQ

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ function calc() {
         massI.textContent = ""; 
     } else {
         lengthI.textContent = `${firstI.value}
-        ${firstI.value > 1 ? "meters" : "meter"} = ${(firstI.value * 3.281).toFixed (3)} feet || 
+        ${firstI.value > 1 ? "meters" : "meter"} = ${(firstI.value * 3.281).toFixed (3)} feet 
+        || 
         ${firstI.value} ${firstI.value > 1 ? "feet" : "foot"} = ${(firstI.value * 0.3048).toFixed(3)}meters`
 
         volumeI.textContent = `${firstI.value}
         ${firstI.value > 1 ? "liters" : "liter"} = ${(firstI.value * 0.264172).toFixed(3)} gallons
-        || ${firstI.value}  ${firstI.value > 1? "gallons" : "gallon"} = ${(firstI.value * 3.78541).toFixed(3)} liters`
+        || 
+        ${firstI.value}  ${firstI.value > 1? "gallons" : "gallon"} = ${(firstI.value * 3.78541).toFixed(3)} liters`
         
         massI.textContent = `${firstI.value}
         ${firstI.value > 1 ? "Kilograms" : "kilogram"} = ${(firstI.value * 2.205).toFixed(3)} pounds
-        || ${firstI.value}  ${firstI.value > 1? "pounds" : "pound"} = ${(firstI.value * 0.454).toFixed(3)}kilograms`
+        || 
+        ${firstI.value}  ${firstI.value > 1? "pounds" : "pound"} = ${(firstI.value * 0.454).toFixed(3)}kilograms`
     }
 
 }
